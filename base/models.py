6fbe49a3925fef8bbaded8e3cba27e9e237d735f
Simardeep Singh <simardeepsngh1@gmail.com>
2024-04-16 16:39:19 +0530

    Rooms Model Created

--- base/models.py
+++ base/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+# Room Model
+class Room(models.Model):
+    # host =
+    # topic =
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # participants =
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name

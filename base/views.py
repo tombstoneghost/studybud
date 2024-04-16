fd8bee8340fd796ecfd8555a3497271c07246510
Simardeep Singh <simardeepsngh1@gmail.com>
2024-04-16 16:09:53 +0530

    Base Application Configured

--- base/views.py
+++ base/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+rooms = [
+    {'id': 1, 'name': 'Lets learn python!'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Frontend Developers'},
+]
+
+
+# Home Page
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+
+# Room
+def room(request, pk):
+    room = None
+
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+
+    context = {'room': room}
+
+    return render(request, 'base/room.html', context)

854b7a801a7b59266195a1c664ee108b829d8273
Simardeep Singh <simardeepsngh1@gmail.com>
2024-04-18 17:05:58 +0530

    Implemented User Authentication

--- base/views.py
+++ base/views.py
@@ -1,9 +1,42 @@
 from django.shortcuts import render, redirect
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
+
+
+# Login Page
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exists')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or Password does not exists')
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+
+# Logout User
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 
 # Home Page

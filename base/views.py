3b5b7efa1678c500d4e103dc2d1e284800985b6e
Simardeep Singh <simardeepsngh1@gmail.com>
2024-04-19 18:50:57 +0530

    Implemented Activity Feed

--- base/views.py
+++ base/views.py
@@ -75,10 +75,10 @@ def home(request):
         Q(description__icontains=q))
 
     topics = Topic.objects.all()
-
     room_count = rooms.count()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
-    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
 
 

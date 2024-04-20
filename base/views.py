df993a5b389741747f287e44b174071453963035
Simardeep Singh <simardeepsngh1@gmail.com>
2024-04-20 15:56:22 +0530

    Implementation of Mobile Responsiveness.

--- base/views.py
+++ base/views.py
@@ -74,7 +74,7 @@ def home(request):
         Q(name__icontains=q) |
         Q(description__icontains=q))
 
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
@@ -203,3 +203,21 @@ def updateUser(request):
 
     context = {'form': form}
     return render(request, 'base/update_user.html', context)
+
+
+# Topics Page
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+
+    context = {'topics': topics}
+    return render(request, 'base/topics.html', context)
+
+
+# Activities Page
+def activityPage(request):
+    room_messages = Message.objects.all()
+
+    context = {'room_messages': room_messages}
+    return render(request, 'base/activity.html', context)
+
